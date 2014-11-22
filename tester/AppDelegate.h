c026f6679dc3ca4cd559e3dd7492a63fec55a380
David Mervine <drex@Davids-MacBook-Air.local>
2014-11-21 18:51:54 -0600

    Initial Commit

---
+++ tester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tester
+//
+//  Created by David Mervine on 11/21/14.
+//  Copyright (c) 2014 David Mervine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tester
+//
+//  Created by David Mervine on 11/21/14.
+//  Copyright (c) 2014 David Mervine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
